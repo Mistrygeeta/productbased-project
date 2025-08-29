388040a9c1fbe15ddd513aebb3d6e87af40890e1
Mistrygeeta <geeta.mistry2001@gmail.com>
2025-08-29 17:27:12 +0530

    feat: initialize project with Express, Mongoose, and basic user model
    - Added package.json with dependencies for Express, Mongoose, and others.
    - Created server.js to set up the Express server and connect to the database.
    - Implemented app.js to configure middleware including cookie-parser.
    - Established database connection in db.js using Mongoose.
    - Defined user model in user.model.js with fields for username, fullname, email, and password.
    - Set up auth.router.js for future authentication routes.

---
+++ src/models/user.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose")
+
+
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true,
+        unique: true
+    },
+    fullname: {
+        firstname:{
+            type: String,
+            required: true
+        },
+        lastname:{
+            type: String
+        }
+    },
+    email:{
+        type: String,
+        unique: true,
+        required: true
+    },
+    password:{
+        type: String,
+        require: true
+    }
+})
+
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
