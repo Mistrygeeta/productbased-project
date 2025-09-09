e4d4c8682682c254458a608fa8f008cb3d07108f
Mistrygeeta <geeta.mistry2001@gmail.com>
2025-09-09 23:16:58 +0530

    add CORS support to enable cross-origin requests

--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@ const express = require("express")
 const cookieParser = require("cookie-parser")
 const authRoutes = require("./routers/auth.route")
 const productRoutes= require("./routers/product.route")
+const cors = require("cors")
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials : true
+}))
 
 app.use("/api/auth",authRoutes)
 app.use("/api/products", productRoutes)
